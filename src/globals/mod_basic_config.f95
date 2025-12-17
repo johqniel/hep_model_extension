76f46ea2e9fe6ae827be2230bfc6f786e8c0e210
johqniel <daniel.nogues.daniel@gmail.com>
2025-12-17 17:43:12 +0100

    Adding different config files. Next -> call program and tell it which config file to use.

--- src/globals/mod_basic_config.f95
+++ src/globals/mod_basic_config.f95
@@ -6,13 +6,16 @@ module mod_basic_config
     !++++++++++++++++++++++++++++++++++++++++++++++++++
     ! Times 
     !--------------------------------------------------
-    integer, parameter :: npops = 1                    ! Single population for testing
+    integer, parameter :: npops = 3                    ! e.g., AMH, NEA, MIX
+                                                       ! MIX = AMH cross with NEA; I assume MIX is same as AMH, having same HEP, rho_max etc 
+                                                       ! The properties are specified in mod_setup.f95
+                                                       !  
 
-    real(8), parameter :: tyr_start(npops) = [-50000.d0]
-    integer, parameter :: tstep_start(npops) = [1]
+    real(8), parameter :: tyr_start(npops) = [-50000.d0, -50000.d0,-50000.d0]
+    integer, parameter :: tstep_start(npops) = [1, 1, 1]
 
-    real(8), parameter :: tyr_end(npops) = [-38000.d0]
-    real(8), parameter :: tyr_length(npops) = [12000.d0]
+    real(8), parameter :: tyr_end(npops) = [-38000.d0, -38000.d0, -38000.d0]
+    real(8), parameter :: tyr_length(npops) = [12000.d0, 12000.d0, 12000.d0]
 
     real(8), parameter :: dt = 0.01
     integer, parameter :: Tn = 1200000
@@ -22,17 +25,17 @@ module mod_basic_config
     integer, parameter :: delta_t_hep = 2000
 
 
-    real(8), parameter :: tau(npops) = [6.d0] 
-    real(8), parameter :: sigma_u(npops) = [15.d0] 
+    real(8), parameter :: tau(npops) = [6.d0, 6.d0, 6.d0] ! Assumed from comments
+    real(8), parameter :: sigma_u(npops) = [10.d0, 10.d0, 10.d0] ! Assumed from comments
 
     
 
-    real(8), parameter :: eta(npops) = [1.6d0]
-    real(8), parameter :: epsilon(npops) = [0.4d0]
-    real(8), parameter :: rho_max(npops) = [5.d0] 
-    real(8), parameter :: r_B(npops) = [0.03d0] 
-    real(8), parameter :: d_B(npops) = [0.02d0] 
-    real(8), parameter :: cb1(npops) = [1250.d0]
+    real(8), parameter :: eta(npops) = [1.6d0, 1.6d0, 1.6d0]
+    real(8), parameter :: epsilon(npops) = [0.4d0, 0.4d0, 0.4d0]
+    real(8), parameter :: rho_max(npops) = [1.d0, 1.d0, 1d0] ! Assumed from comments
+    real(8), parameter :: r_B(npops) = [0.01d0, 0.01d0, 0.01d0] ! Assumed from comments
+    real(8), parameter :: d_B(npops) = [0.025d0, 0.025d0, 0.025d0] ! Assumed from comments
+    real(8), parameter :: cb1(npops) = [1250.d0, 1250.d0, 1250.d0]
     
 
     real(8), parameter :: dt_bdyr = 1.
@@ -50,13 +53,21 @@ module mod_basic_config
 
     integer, parameter :: ns = 4
 
-    real(8), parameter :: x_ini_c(ns, npops) = reshape([4.0d0, 4.0d0, 4.0d0, 4.0d0], [ns, npops])
+    real(8), parameter :: x_ini_c(ns, npops) = reshape([4.0d0, 4.0d0, 4.0d0, 4.0d0, &
+                                                        -8.6d0, -3.93d0, -0.72d0, -5.54d0, &
+                                                        4.0d0, 4.0d0, 4.0d0, 4.0d0], [ns, npops])
                                                         
-    real(8), parameter :: y_ini_c(ns, npops) = reshape([44.08d0, 44.08d0, 44.08d0, 44.08d0], [ns, npops])
+    real(8), parameter :: y_ini_c(ns, npops) = reshape([44.08d0, 44.08d0, 44.08d0, 44.08d0, &
+                                                        39.2d0, 42.3d0, 40.03d0, 36.66d0, &
+                                                        44.08d0, 44.08d0, 44.08d0, 44.08d0], [ns, npops])
                                                         
-    real(8), parameter :: ini_spread(ns, npops) = reshape([0.5d0, 0.5d0, 0.5d0, 0.5d0], [ns, npops])
+    real(8), parameter :: ini_spread(ns, npops) = reshape([0.5d0, 0.5d0, 0.5d0, 0.5d0, &
+                                                           1.0d0, 1.0d0, 1.0d0, 1.0d0, &
+                                                           0.5d0, 0.5d0, 0.5d0, 0.5d0], [ns, npops])
 
-    integer, parameter :: hum_0(ns, npops) = reshape([200, 200, 200, 200], [ns, npops])
+    integer, parameter :: hum_0(ns, npops) = reshape([200, 200, 200, 200, &
+                                                      150, 150, 150, 150, &
+                                                      0, 0, 0, 0], [ns, npops])
 
 
 
@@ -89,6 +100,8 @@ module mod_basic_config
     ! HEP Input Files
     !++++++++++++++++++++++++++++
     character(len=256), parameter :: hep_paths(npops) = [character(len=256) :: &
-        "input/hep/europe/AUR.nc"]
+        "input/hep/europe/AUR.nc", &
+        "input/hep/europe/NEA.nc", &
+        "input/hep/europe/MIX.nc"]
 
 end module mod_basic_config
